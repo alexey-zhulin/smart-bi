5c282838b4dc69e7bd9adc5fa7358ccf8f4d1db5
Alexey <alexey.zhulin@gmail.com>
2015-05-14 23:20:58 +0500

    daily commit

--- src/ru/smart_bi/gui_forms/DictionaryForm.java
+++ src/ru/smart_bi/gui_forms/DictionaryForm.java
@@ -1,10 +1,26 @@
 package ru.smart_bi.gui_forms;
 
+import javax.swing.JScrollPane;
+
+import ru.smart_bi.data_models.DictionaryTableModel;
+import ru.smart_bi.gui_tools.JTreeTable;
+import ru.smart_bi.object_instances.DictionaryInstance;
+
 public class DictionaryForm extends TemplateFrame {
 	
 	private static final long serialVersionUID = 1595700307570105920L;
+	private JTreeTable treeTable;
+	private DictionaryInstance dictionaryInstance;
 
-	public DictionaryForm() {
-		
+	public DictionaryForm(DictionaryInstance dictionaryInstance) {
+		this.treeTable = new JTreeTable(new DictionaryTableModel(dictionaryInstance));
+		this.dictionaryInstance = dictionaryInstance;
+	}
+	
+	public void ShowForm() {
+		setTitle((String) dictionaryInstance.dictionaryDescriptor.object_name);
+		getContentPane().add(new JScrollPane(treeTable));
+		pack();
+		setVisible(true);
 	}
 }
